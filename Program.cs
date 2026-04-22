636992e4cf0d66b37343786e328db098463f692b
MEO-KB-lessons <tum_ms@mail.ru>
2026-04-22 19:17:38 +0700

    fixed code

--- Program.cs
+++ Program.cs
@@ -1,25 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Test application
+
+Console.WriteLine("Hello, WORLD!");
+
 
 // Реверс строки 
 
 static string ReverseString(string input)
 {
     // Способ 1: Использование массива символов
-    char[] charArray = input.ToCharArray();
-    Array.Reverse(charArray);
-    return new string(charArray);
+    //char[] charArray = input.ToCharArray();
+    //Array.Reverse(charArray);
+    **return new string(charArray);
     
     // Способ 2: Вручную через цикл
-    // string result = "";
-    // for (int i = input.Length - 1; i >= 0; i--)
-    // {
-    //     result += input[i];
-    // }
-    // return result;
+    string result = "";
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        result += input[i];
+    }
+    return result;
 }
     
 string input = "Строка для тестирования программы";
 Console.WriteLine($"Исходная строка: {input}");
 Console.WriteLine($"Перевернутая строка: {ReverseString(input)}");
-
-
