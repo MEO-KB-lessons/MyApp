97cf51ea229cf1647dbeccf35f3dc2b2125292fd
MEO-KB-lessons <tum_ms@mail.ru>
2026-04-22 18:42:47 +0700

    Add reverse string function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// Реверс строки 
+
+static string ReverseString(string input)
+{
+    // Способ 1: Использование массива символов
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+    
+    // Способ 2: Вручную через цикл
+    // string result = "";
+    // for (int i = input.Length - 1; i >= 0; i--)
+    // {
+    //     result += input[i];
+    // }
+    // return result;
+}
+    
+string input = "Строка для тестирования программы";
+Console.WriteLine($"Исходная строка: {input}");
+Console.WriteLine($"Перевернутая строка: {ReverseString(input)}");
+
+
